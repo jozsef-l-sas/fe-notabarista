43a646ee55019f0c5bc11ea4ca2ac125e1cc4495
Jozsef Sas <jozsef.l.sas@gmail.com>
2021-02-28 09:36:42 +0200

    #19 Angular demo integration with NAB backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/api/api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fe-notabarista';
+  userInfo: any = '';
+
+  constructor(private apiService: ApiService) {
+
+  }
+
+  showUserInfo() {
+    this.apiService.getUserInfo()
+      .subscribe((data) => {
+        this.userInfo = data;
+      });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth-interceptor.service';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useFactory: function() {
+          return new AuthInterceptor();
+      },
+      multi: true,
+      deps: []
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
